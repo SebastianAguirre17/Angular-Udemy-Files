23a72486825ac704d5e137266efd0aa6a494a539
sebastianaguirre17 <sebaaguirre2012@gmail.com>
2020-10-17 15:58:20 -0300

    Proyecto finalizado

--- src/app/services/carga-imagen.service.ts
+++ src/app/services/carga-imagen.service.ts
@@ -13,11 +13,33 @@ export class CargaImagenService {
     constructor(private db: AngularFirestore) { }
 
     cargarImagenesFirebase(imagenes: FileItem[]) {
-        console.log(imagenes);
+        const storageRef = firebase.storage().ref();
+        for(const item of imagenes) {
+            item.subiendo = true;
+            if(item.progreso >= 100) {
+                continue;
+            }
+
+            const uploadTask: firebase.storage.UploadTask = storageRef.child(`${this.CARPETA_IMAGENES}/${item.nombre}`)
+                                                                        .put(item.archivo);
+            uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+                (snapshot: firebase.storage.UploadTaskSnapshot) => item.progreso = (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+                (err) => console.error('Error al subir', err),
+                async () => {
+                    console.log('Imagen cargada correctamente');
+                    item.url = await uploadTask.snapshot.ref.getDownloadURL();
+                    this.guardarReferenciaImagen({
+                        nombre: item.nombre, 
+                        url: item.url
+                    });
+                }
+            )
+        }
     }
 
-    private guardarImagen(imagen: {nombre: string, url: string}) {
-        this.db.collection(`${this.CARPETA_IMAGENES}`)
+    private guardarReferenciaImagen(imagen: {nombre: string, url: string}) {
+        console.log('Aca fallas');
+        this.db.collection(`/${this.CARPETA_IMAGENES}`)
                 .add(imagen);
     }
 }
